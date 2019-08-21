19fdfc4714615faf82e940349fe97d0433a883d1
monambigha <monambigha@email.arizona.edu>
2019-08-21 12:01:29 -0700

    front end edits

--- frontend/main.py
+++ frontend/main.py
@@ -1,33 +1,32 @@
 from flask import Flask, render_template, request, redirect, abort, jsonify
-# import requests
+import requests
 # from datetime import datetime
 # import re
-# import os
-# import uuid
+import os
+import uuid
 # import auth
 # import json
 # from collections import namedtuple
-# try:
-#   import googleclouddebugger
-#   googleclouddebugger.enable()
-# except ImportError:
-#   pass
+try:
+    import googleclouddebugger
+    googleclouddebugger.enable()
+except ImportError:
+    pass
 
 app = Flask(__name__)
 
-# try:
-#   app.config.from_object(os.environ['APP_SETTINGS'])
-#   URL = app.config['API']
-#   BUCKET = app.config['BUCKET']
-#   PROJECT_NAME = app.config['PROJECT']
-# except:
-#   # This occurs when testing locally from the command line
-#   #  - if no env variable is set, these defaults are set 
-#   # Port is 5000 because only reason for running from command line locally
-#   # is because backend is being debugged in vscode, in which case it is on port 5000
-#   URL = 'http://localhost:5000'
-#   PROJECT_NAME = 'roi-hiplocal'
-#   BUCKET = 'roi-hiplocal'
+try:
+    app.config.from_object(os.environ['APP_SETTINGS'])
+    URL = app.config['API']
+    BUCKET = app.config['BUCKET']
+    PROJECT_NAME = app.config['PROJECT']
+except:
+    #    This occurs when testing locally from the command line
+    #  - if no env variable is set, these defaults are set 
+    #  Port is 5000 because only reason for running from command line locally
+    # is because backend is being debugged in vscode, in which case it is on port 5000
+    URL = 'http://35.202.136.214:5000'
+    PROJECT_NAME = 'events-feed-deloitte'
 
 # @app.route("/")
 # def home():
@@ -41,16 +40,24 @@ app = Flask(__name__)
 #   model['greeting'] = os.getenv('GREETING', 'Welcome to Events feed')
 #   return render_template("home.html", model=model, bucket=BUCKET)
 
+
 @app.route("/")
 def showEvents():
-#   try:
-#     auth.authorize(request)
-#   except Exception: 
-#       pass
-#       return redirect('/login')
     return render_template("login.html")
 
 
 @app.route("/addEvent")
 def addEvent():
     return render_template("createEvent.html")
+
+
+@app.route("/events/add", methods=['POST'])
+def add_event():
+    url = 'http://35.202.136.214:80' + '/events/add/' + str(uuid.uuid4())
+    data = request.form.to_dict(flat=True)
+    print(requests.post(url, data=data))
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
