51094d495ffb402917b967522f74c82af66c4100
asriprakash <asriprakash@deloitte.com>
2019-08-21 11:25:51 -0500

    backend completed

--- backend/db.py
+++ backend/db.py
@@ -5,12 +5,31 @@ import datetime
 EVENTS = firestore.Client().collection('events')
 
 
-def add_events(id):
-    print('adding a new event' + id + ' to firestore')
-    doc_ref = EVENTS.document(id)
-    doc_ref.set({
-        u'eventDesc': u'Dummy',
-        u'eventName': u'Lovelace'
-    })
-    print ("success")
+# def add_events(id):
+#     print('adding a new event' + id + ' to firestore')
+#     doc_ref = EVENTS.document(id)
+#     doc_ref.set({
+#         u'eventDesc': u'Dummy',
+#         u'eventName': u'Lovelace',
+#         u'eventsDate': u'8/12/2019',
+#     })
+#     print ("success")
+#     return id
+
+def add_events(data, id):
+    print('adding a new happening' + id + ' to firestore')
+    data['eventDate'] = datetime.datetime.now()
+    EVENTS.document(id).set(data)
     return id
+
+
+def get_events():
+    print('Retrieving events from firestore')
+    docs = EVENTS.order_by('eventDate', direction=firestore.Query.DESCENDING).get()
+    ret = []
+    for doc in docs:
+        d = doc.to_dict()
+        d['id'] = doc.id
+        ret.append(d)
+    return ret
+
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import db
+import json
 
 app = Flask(__name__)
 
@@ -11,8 +12,17 @@ def default_events():
 
 @app.route('/events/add/<id>', methods=['POST'])
 def add_events(id):
-    event = db.add_events(id)
+    data = Flask.request.form.to_dict(flat=True)
+    event = db.add_events(data, id)
+    return Flask.jsonify({'id': event}), 201
     return event
+
+
+@app.route('/events', methods=['GET'])
+def get_events():
+    print('backend service responding to request for events')
+    # string is default to prevent error when jsonifying python datetime
+    return json.dumps(db.get_events(), indent=4, sort_keys=True, default=str), 200
 
 
 if __name__ == '__main__':
